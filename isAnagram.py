063921b55844b56ef3c6ddb5c4f68284ec16608c
ojaljain18 <134694413+ojaljain18@users.noreply.github.com>
2023-08-19 14:44:05 +0530

    Create isAnagram.py

---
+++ isAnagram.py
@@ -0,0 +1,24 @@
+from collections import Counter
+def isAnagram(s:str,t:str):
+    print(sorted(s))
+    print(sorted(t))
+   
+    print(Counter(s))
+    print(Counter(t))
+    
+    if len(s)!=len(t):
+        return False
+    
+    countS,countT={},{}
+    
+    for i in range(len(s)):
+       
+        countS[s[i]]=1+countS.get(s[i],0)
+        countT[t[i]]=1+countT.get(t[i],0)
+        
+    for c in countS:
+        
+        if countS[c] != countT.get(c,0):
+            return False
+    
+print(isAnagram('rat','tam'))
